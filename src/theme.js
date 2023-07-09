c213afbd12f8e506e151ab568e493b1e009cbb60
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-07-09 18:10:03 +0300

    feat: create theme file

---
+++ src/theme.js
@@ -0,0 +1,58 @@
+import { createTheme } from '@mui/material/styles';
+
+const colors = {
+    primary: '#1B4B66',
+    primaryTint: '#639599',
+    error: '#B00020',
+    highlight: '#FF8C4B',
+    accent: '#F4F4F4',
+    dark: '#13101E',
+    bright: '#FFFFFF',
+    grey: '#F1F1F1',
+    lightText: '#B6B6B6',
+    typeHightEmphasis: '#171520',
+    typeLowEmphasis: '#626262',
+};
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: colors.primary,
+        },
+        primaryTint: {
+            main: colors.primaryTint,
+        },
+        error: {
+            main: colors.error,
+        },
+        highlight: {
+            main: colors.highlight,
+        },
+        accent: {
+            main: colors.accent,
+        },
+        dark: {
+            main: colors.dark,
+        },
+        bright: {
+            main: colors.bright,
+        },
+        grey: {
+            main: colors.grey,
+        },
+        lightText: {
+            main: colors.lightText,
+        },
+        typeHightEmphasis: {
+            main: colors.typeHightEmphasis,
+        },
+        typeLowEmphasis: {
+            main: colors.typeLowEmphasis,
+        },
+    },
+    typography: {
+        fontFamily: 'Inter, sans-serif',
+    },
+});
+
+export default theme;
