084da30fbaf014f7de0987aeb364ff2952b2e84f
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-07-09 18:10:17 +0300

    Wrap the entry point by themeProvider

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
