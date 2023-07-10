027bea9f3c374d0a1c75bb0ab4d69bd8eaf89db8
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-07-10 20:42:25 +0300

    clean import & export

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
 export { default as TitledList } from './titledLists/TitledList.jsx';
+export { default as Footer } from './footer/Footer.jsx';
+export { default as SocialIcon } from './socialIcon/SocialIcon.jsx';
