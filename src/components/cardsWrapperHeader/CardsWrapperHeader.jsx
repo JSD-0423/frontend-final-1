1a18e5d4c1890aebfeebb303fb1532fde3f68c8a
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-07-15 00:14:04 +0300

    Add style to wrapper components

--- src/components/cardsWrapperHeader/CardsWrapperHeader.jsx
+++ src/components/cardsWrapperHeader/CardsWrapperHeader.jsx
@@ -1,18 +1,59 @@
-import { Stack, Typography, useTheme } from '@mui/material'
-import React from 'react'
-import IconBtn from '../iconBtn/IconBtn'
+import { Stack, Typography, useTheme } from "@mui/material";
+import React from "react";
+import IconBtn from "../iconBtn/IconBtn";
 
-const CardsWrapperHeader = ({wrapperTitle, fontColor, WrapperGoToIcon, WrapperPseudoIconId}) => {
-    const theme = useTheme();
+const CardsWrapperHeader = ({
+  wrapperTitle,
+  fontColor,
+  WrapperGoToIcon,
+  WrapperPseudoIconId,
+  style,
+}) => {
+  const theme = useTheme();
   return (
-    <Stack direction='row' justifyContent='space-between' alignItems='center' mb={2}>
-        <Typography component='h2' fontSize={{ xs:'16px', md:'34px'}} fontWeight='600' lineHeight={{xs:'16px', md:'44px'}} color={fontColor} >{wrapperTitle}</Typography>
-        <Stack direction='row' justifyContent='space-between' alignItems='center'>
-            <Typography variant='caption' fontWeight='600' lineHeight={{xs:'18px', md:'24px'}} color={theme.palette.primary.main}>View All</Typography>
-            <IconBtn iconsNames={WrapperGoToIcon} pseudoIconId={WrapperPseudoIconId} iconPadding={0} iconSize={{xs:'20px', md:'30px'}} iconColor={{xs:theme.palette.primary.main, md:fontColor}} pseudoType={'&:hover'} pseudoBackgroundColor='transparent' />
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      mb={2}
+      style={style}
+    >
+      <Typography
+        component="h2"
+        fontSize={{ xs: "16px", md: "34px" }}
+        fontWeight="600"
+        lineHeight={{ xs: "16px", md: "44px" }}
+        color={fontColor}
+      >
+        {wrapperTitle}
+      </Typography>
+      {WrapperGoToIcon && WrapperPseudoIconId && (
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography
+            variant="caption"
+            fontWeight="600"
+            lineHeight={{ xs: "18px", md: "24px" }}
+            color={theme.palette.primary.main}
+          >
+            View All
+          </Typography>
+          <IconBtn
+            iconsNames={WrapperGoToIcon}
+            pseudoIconId={WrapperPseudoIconId}
+            iconPadding={0}
+            iconSize={{ xs: "20px", md: "30px" }}
+            iconColor={{ xs: theme.palette.primary.main, md: fontColor }}
+            pseudoType={"&:hover"}
+            pseudoBackgroundColor="transparent"
+          />
         </Stack>
+      )}
     </Stack>
-  )
-}
+  );
+};
 
-export default CardsWrapperHeader
+export default CardsWrapperHeader;
