989e8558f892942263e7554f07366b5dda456869
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-07-16 00:50:39 +0300

    Shop by brands component

---
+++ src/components/ImageCard/ImageCard.jsx
@@ -0,0 +1,31 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const ImageCard = ({ image }) => {
+  return (
+    <Box
+      sx={{
+        borderRadius: "16px",
+        backgroundColor: "accent.main",
+        width: ["100px", "133px", "168px"],
+        height: ["100px", "133px", "168px"],
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        WebkitFlexBasis: ["29%", "29.3%", "13.3%"],
+      }}
+    >
+      <Box
+        component="img"
+        src={image}
+        alt={image}
+        sx={{
+          width: ["60px", "70px", "90px", "109px"],
+          height: ["25px", "35px", "46px"],
+        }}
+      />
+    </Box>
+  );
+};
+
+export default ImageCard;
