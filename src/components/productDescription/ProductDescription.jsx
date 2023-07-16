4d1c4f6810b2a6f7b2ce3260f3ba7aeb245c2be0
AymanQ-A-Maani <maaniayman@gmail.com>
2023-07-16 21:59:02 +0300

    initialized product description component

---
+++ src/components/productDescription/ProductDescription.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const ProductDescription = ({pName, pDesc, pRating, pPrice, pOldPrice, pSpecialOffer}) => {
+  return (
+    <Box width='50%' justifyContent='flex-end'>
+
+
+    </Box>
+  )
+}
+
+export default ProductDescription
--- src/pages/Pdp.jsx
+++ src/pages/Pdp.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import CustomContainer from '../components/customContainer/CustomContainer'
+import ProductDescription from '../components/productDescription/ProductDescription'
+import { Stack } from '@mui/material'
 
 const Pdp = () => {
   return (
     <CustomContainer>
+      <Stack direction='row' > 
+        <ProductDescription />
+      </Stack>
     </CustomContainer>
   )
 }
