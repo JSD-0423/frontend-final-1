2afb63591b4bd569df6ac756c31c1f9b445f80f1
AymanQ-A-Maani <maaniayman@gmail.com>
2023-07-19 19:26:25 +0300

    initialized cateogry page

---
+++ src/pages/Category.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {CustomContainer} from "../components"
+import { Box } from '@mui/material'
+const Category = () => {
+  return (
+    <CustomContainer>
+        <Box>
+            
+        </Box>
+    </CustomContainer>
+  )
+}
+
+export default Category
