0f1835ee67acf8fb73f938d6981d0e89508b95da
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2023-07-29 22:47:24 +0300

    Add Cart Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Home } from './Home';
 export { default as ProductDetails } from './ProductDetails';
 export { default as Category } from './Category';
 export { default as Checkout } from './Checkout';
+export { default as Cart } from './Cart';
