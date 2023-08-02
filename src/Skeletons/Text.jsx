ad348f14653cac1afa35ea460c82d900126df717
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2023-08-02 18:22:47 +0300

    Create Text Skeleton

---
+++ src/Skeletons/Text.jsx
@@ -0,0 +1,8 @@
+import { Skeleton } from "@mui/material";
+import React from "react";
+
+const Text = () => {
+  return <Skeleton variant="text" sx={{ fontSize: "2rem", width: "100%" }} />;
+};
+
+export default Text;
---
+++ src/Skeletons/index.jsx
@@ -0,0 +1,1 @@
+export { default as TextSkeleton } from "./Text";
