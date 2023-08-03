cb8404513e4fad6262b4127282c08c4ddc4e5e69
AymanQ-A-Maani <maaniayman@gmail.com>
2023-08-03 07:29:55 +0300

    fixed key prop for products skeleton

--- src/Skeletons/Products.jsx
+++ src/Skeletons/Products.jsx
@@ -4,25 +4,25 @@ import React from "react";
 const Products = ({width,height,numberOfItems}) => {
   return (
     <>
-      {new Array(numberOfItems).fill(
-        <Stack width={width} height={height}  direction='column' justifyContent="space-between" flexWrap="nowrap">
-          <Skeleton variant="rounded" width={width} height={height} />
-          <Box
-            sx={{
-              pt: 0.5,
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            <Box width="100%">
-              <Skeleton width="40%" />
-              <Skeleton width="50%" />
-              <Skeleton width="40%" />
-            </Box>
-            <Skeleton width={20} height={20} variant="circular" />
-          </Box>
-        </Stack>
-      )}
+    {new Array(numberOfItems).fill(null).map((_, index) => (
+      <Stack key={index} width={width} height={height}  direction='column' justifyContent="space-between" flexWrap="nowrap">
+      <Skeleton variant="rounded" width={width} height={height} />
+      <Box
+        sx={{
+          pt: 0.5,
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box width="100%">
+          <Skeleton width="40%" />
+          <Skeleton width="50%" />
+          <Skeleton width="40%" />
+        </Box>
+        <Skeleton width={20} height={20} variant="circular" />
+      </Box>
+    </Stack>
+    ))}
     </>
   );
 };
